40b073fe06512617216908d0f791022a540c82b0
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-04-29 23:38:57 +0200

    Add public static path

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
